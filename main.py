30f4fa60f65693c394a757d33ea1c27d713a8c1e
ebomb07 <66098261+ebomb07@users.noreply.github.com>
2023-10-22 22:26:59 -0400

    update & patch
    Updated and patched ceaser_cipher.py and main.py

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import os
 
 
 clear = lambda: os.system('cls')
-
-clear()
-print("Welcome what kind of cipher would you like to use?\n\n")
-
-x = int(input(("[1] ceaser cipher\n\n---> ")))
-
-if x == 1:
+active = True
+while active:
     clear()
-    subprocess.run(['python','ceaser_cipher.py'])
+    print("Welcome what kind of cipher would you like to use?\n\n")
+
+    x = int(input(("[1] ceaser cipher\n\n---> ")))
+
+    if x == 1:
+        clear()
+        subprocess.run(['python','ceaser_cipher.py'])
+        active = False
+    else:
+        pass
