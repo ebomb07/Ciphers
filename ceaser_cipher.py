cb6d3f1880985e4d759ede804054d0c715e38d66
ebomb07 <66098261+ebomb07@users.noreply.github.com>
2023-10-22 21:27:53 -0400

    Update ceaser_cipher.py

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,31 +1,27 @@
-
-
-x = 0
-n = 0
 fArray = []
 arr = []
+finalArray = []
+
 
 letters = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"]
 
 letters += "s","t","u","v","w","x","y","z"
 
-letters += "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S"
-letters += "T","U","V","W","X","Y","Z"
 
 
 
 start = int(input("[1] Encrypt\n[2] Decrypt\n[3] Brute force\n---> "))
 
-
-
-    
-
-
+def string_maker(a_list):
+    ret_string = ""
+    for val in a_list:
+        ret_string = ret_string + val
+    return ret_string
 
 #encrypt
 
 if start == 1:
-    user_input = str(input("What is your string\n"))
+    user_input = str(input("What is your string\n")).lower()
     shift = str(input("What is your shift\n"))
     array = list(user_input)
 
@@ -54,7 +50,7 @@ if start == 1:
 #decrypt
 
 if start == 2:
-    user_input = str(input("What is your string\n"))
+    user_input = str(input("What is your string\n")).lower()
     shift = str(input("What is your shift\n"))
 
     array = list(user_input)
@@ -76,39 +72,39 @@ if start == 2:
         print(fArray)
 
 
-    print("ceaser cipher ecryption: ") 
+    print("ceaser cipher ecryption: \n") 
     print(*fArray, sep = "")
 
 
 #Brute force
 
 if start == 3:
-    shift = 0
-    user_input = str(input("What is your string\n"))
+    user_input = str(input("What is your string\n")).lower()
 
     array = list(user_input)
 
-    print("\n\noriginal: " + user_input+"\n")
-
-    arrayLength = range(len(array))
-
+    print("\n\noriginal: \"" + user_input+"\"\n")
     fArray = list(arr)
-
+    arrayLength = range(len(array))
+    cipherList = {}
+            
+    shift = 0
+    i = 0
 
     for x in arrayLength:
+        intArr = letters.index(array[x])
+        fArray.append(intArr)
 
-        while n <= 52:
-            shift = 1
-            intArr = letters.index(array[x])
 
-            while shift <= len(letters):
+    fArrayLen = range(len(fArray))
+    for j in range(26):
+        temp = []
+        for i in  fArrayLen:
+        
+            array = letters[fArray[i] - int(j)]
+            temp.append(array)
+        cipherList[j] = temp
 
-                
-                arr = (letters[(intArr - int(shift))])
-                fArray.extend(arr)
-
-                print("["+str(shift)+"] " + str(fArray))
-                shift += 1
-
-            n += 1
-    
+    for key, value in cipherList.items():
+        print(str(key) +": "+ str(string_maker(value)) +"\n")
+        
